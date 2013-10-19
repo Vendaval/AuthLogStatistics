2b7fea68f705529fe48f5ec6923c1eb9df319cbe
Alberto Rodriguez S. A.K.A Vendaval <hotgalan@gmail.com>
2013-10-18 20:52:05 -0500

    some changes

--- bp.sh
+++ bp.sh
@@ -6,32 +6,41 @@ logdir="$logbasedir"/$(date +%F)
 mkdir -p "$logdir"
 
 tmpfile="/tmp/breakinattempts.txt"
-
-
-
-logfile="$logdir/invalid_passwords.txt"
-zgrep -i -v "Failed password for invalid user" $logbasedir/auth.log.* | grep -i "Failed password" > "$tmpfile"
-cat "$tmpfile" | cut -d " " -f 10 | sort | uniq | while read line ; do
-    echo -n "$line "; cat "$tmpfile" | grep "$line" | wc -l;
-done | sort -n -k 2 >"$logfile"
-
 rm $tmpfile
 
-echo "Created $logfile with the absolute frequency of break-in attempts with an existing user name but an invalid password."
+#logfile="$logdir/invalid_passwords.txt"
+#zgrep -i -v "Failed password for invalid user" $logbasedir/auth.log* | grep -i "Failed password" > "$tmpfile"
+#cat "$tmpfile" | cut -d " " -f 10 | sort | uniq | while read line ; do
+#    echo -n "$line "; cat "$tmpfile" | grep "$line" | wc -l;
+#done | sort -n -k 2 >"$logfile"
+#rm $tmpfile
+#echo "Created $logfile with the absolute frequency of break-in attempts with an existing user name but an invalid password."
 
-echo "L..."
+echo "Interlude ..."
 
-logfile="$logdir/invalid_users.txt"
+#logfile="$logdir/invalid_users.txt"
+#zgrep -i "Failed password for invalid user" $logbasedir/auth.log* > "$tmpfile"
+#cat "$tmpfile" | cut -d " " -f 11 | sort | uniq | while read line ; do
+#    echo -n "$line "; cat "$tmpfile" | grep "$line" | wc -l;
+#done | sort -n -k 2 >"$logfile"
+#rm "$tmpfile"
+#echo "Created $logfile with the absolute frequency of break-in attempts with a non-existing user name."
 
-echo "L..."
+echo "Interlude ... 2"
 
-zgrep -i "Failed password for invalid user" $logbasedir/auth.log.* > "$tmpfile"
-cat "$tmpfile" | cut -d " " -f 11 | sort | uniq | while read line ; do
+#logfile="$logdir/ip_attempts.txt"
+#zgrep -i "sshd" $logbasedir/auth.log*  | grep -i "authentication failure" | sed 's/^.*rhost=//' > "$tmpfile"
+#cat "$tmpfile" | cut -d " " -f 1 | sort | uniq | while read line ; do
+#    echo -n "$line "; cat "$tmpfile" | grep "$line" | wc -l;
+#done | sort -n -k 2 >"$logfile"
+#rm $tmpfile
+#echo "Created $logfile with the absolute frequency of break-in attempts with an existing user name but an invalid password."
+
+echo "Interlude ... 3"
+logfile="$logdir/perCountry_attempts.txt"
+zgrep -i "sshd" $logbasedir/auth.log*  | grep -i "authentication failure" | sed 's/^.*rhost=//' > "$tmpfile"
+cat "$tmpfile" | cut -d " " -f 1 | sort | uniq | while read line ; do
     echo -n "$line "; cat "$tmpfile" | grep "$line" | wc -l;
 done | sort -n -k 2 >"$logfile"
-
-echo "L..."
-
-rm "$tmpfile"
-
-echo "Created $logfile with the absolute frequency of break-in attempts with a non-existing user name."
+#rm $tmpfile
+echo "Created $logfile with the absolute frequency of break-in attempts with an existing user name but an invalid password."
